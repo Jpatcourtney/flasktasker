a37d65c324b56458dcfe57f1c0819077913ef8cb
John Courtney <jpatcourtney@gmail.com>
2016-05-19 08:39:33 -0400

    testing

--- db_migrate.py
+++ db_migrate.py
@@ -2,26 +2,47 @@ from views import db
 from config import DATABASE_PATH
 
 import sqlite3
-from datetime import datetime
+#from datetime import datetime
+
+# with sqlite3.connect(DATABASE_PATH) as connection:
+#     c = connection.cursor()
+
+#     #move tasks table to new table
+#     c.execute("ALTER TABLE tasks RENAME TO old_tasks")
+
+#     #create tables again (makes new tasks table)
+#     db.create_all()
+
+#     #grab what was on the old table
+#     c.execute("SELECT name, due_date, priority, status FROM old_tasks ORDER BY task_id ASC")
+
+#     #save all the rows as a list of tuples; set posted_date to now and user_id to 1
+#     data = [(row[0], row[1], row[2], row[3], datetime.now(), 1) for row in c.fetchall()]
+
+#     #insert data to tasks table
+#     c.executemany("INSERT INTO tasks (name, due_date, priority, status, posted_date, user_id) VALUES (?, ?, ?, ?, ?, ?)", data)
+
+#     #delete the olds tasks table
+#     c.execute("DROP TABLE old_tasks")
 
 with sqlite3.connect(DATABASE_PATH) as connection:
+
+    # cursor
     c = connection.cursor()
 
-    #move tasks table to new table
-    c.execute("ALTER TABLE tasks RENAME TO old_tasks")
+    # change table name
+    c.execute('ALTER TABLE users RENAME TO old_users')
 
-    #create tables again (makes new tasks table)
+    # recreate tables
     db.create_all()
 
-    #grab what was on the old table
-    c.execute("SELECT name, due_date, priority, status FROM old_tasks ORDER BY task_id ASC")
+    # get old data
+    c.execute('SELECT name, email, password FROM old_users ORDER BY id ASC')
 
-    #save all the rows as a list of tuples; set posted_date to now and user_id to 1
-    data = [(row[0], row[1], row[2], row[3], datetime.now(), 1) for row in c.fetchall()]
+    # save data as list of tuples and add user role
+    data = [(row[0], row[1], row[2], 'user') for row in c.fetchall()]
 
-    #insert data to tasks table
-    c.executemany("INSERT INTO tasks (name, due_date, priority, status, posted_date, user_id) VALUES (?, ?, ?, ?, ?, ?)", data)
+    # insert data to users table
+    c.executemany('INSERT INTO users (name, email, password, role) VALUES (?,?,?,?)', data)
 
-    #delete the olds tasks table
-    c.execute("DROP TABLE old_tasks")
-
+    c.execute('DROP TABLE old_users')
--- forms.py
+++ forms.py
@@ -1,21 +1,39 @@
 from flask_wtf import Form
 from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class AddTaskForm(Form):
     task_id = IntegerField()
     name = StringField('Task Name', validators=[DataRequired()])
-    due_date = DateField('Due Date (mm/dd/yyyy)', validators=[DataRequired()], format="%m/%d/%y")
-    priority = SelectField('Priority', validators=[DataRequired()], choices=[('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8'),('9','9'),('10','10')])
+    due_date = StringField('Due Date (mm/dd/yyyy)')
+    #due_date = DateField('Due Date (mm/dd/yyyy)', validators=[DataRequired()], format='%m/%d/%y')
+    priority = SelectField(
+        'Priority', 
+        validators=[DataRequired()], 
+        choices=[
+            ('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),
+            ('6','6'),('7','7'),('8','8'),('9','9'),('10','10')])
     status = IntegerField('Status')
 
 class RegisterForm(Form):
-    name = StringField('Username', validators=[DataRequired(), Length(min=6, max=25)])
-    email = StringField('Email', validators=[DataRequired(), Length(min=6, max=40)]) 
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
-    confirm = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password', message="Passwords didn't match")])
+    name = StringField(
+        'Username', 
+        validators=[DataRequired(), Length(min=6, max=25)])
+    email = StringField(
+        'Email', 
+        validators=[DataRequired(), Email(), Length(min=6, max=40)]) 
+    password = PasswordField(
+        'Password', 
+        validators=[DataRequired(), Length(min=6, max=40)])
+    confirm = PasswordField(
+        'Repeat Password', 
+        validators=[DataRequired(), EqualTo('password', message="Passwords didn't match")])
 
 class LoginForm(Form):
-    name = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    name = StringField(
+        'Username', 
+        validators=[DataRequired()])
+    password = PasswordField(
+        'Password', 
+        validators=[DataRequired()])
 
